bb78e962818ebc39f299bb62f21e509dcb10f11a
Brian Roepke <broepke@gmail.com>
2024-12-23 09:14:56 -0600

    Added streamline auth

--- Home.py
+++ Home.py
@@ -1,57 +1,98 @@
-import random
 import streamlit as st
+import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities import LoginError
+import yaml
+from yaml.loader import SafeLoader
 import ldclient
 from ldclient import Context
 from ldclient.config import Config
+
+st.title("Hello, Streamlit!")
+st.code("Username: brian \ chris \nPassword: brian \ chris")
 
 # Get the LaunchDarkly SDK key from Streamlit Secrets
 sdk_key = st.secrets["other"]["launchdarkly_sdk_key"]
 ldclient.set_config(Config(sdk_key))
 
-# Define a feature flag key
-FEATURE_FLAG_KEY = "new-homepage"
+# Load credentials from the YAML file
+with open("config.yaml") as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-cohort = random.choice(["A", "B"])
-if cohort == "A":
-    # Define the user context for cohort A
-    user_key = "532169c2-a050-4210-841c-9ecc95a22cc8"
-    user_name = "Jane Doe"
-    user_email = "jane.doe@example.com"
-    user_gender = "Female"
-else:
-    # Define the user context for cohort B
-    user_key = "b2e0c2b4-5e0e-4f2b-8c2f-8f0e4e2b5f2b"
-    user_name = "John Doberman"
-    user_email = "john.dobs@meglocorp.com"
-    user_gender = "Male"
+# Uncomment this plus the file write at the end of this script
+# stauth.Hasher.hash_passwords(config['credentials'])
 
-# Build the user context
-builder = Context.builder(user_key)
-builder.kind("user")
-builder.name(user_name)
-builder.set("email", user_email)
-builder.set("gender", user_gender)
-context = builder.build()
+# Initialize the authenticator
+authenticator = stauth.Authenticate(
+    config["credentials"],
+    config["cookie"]["name"],
+    config["cookie"]["key"],
+    config["cookie"]["expiry_days"],
+)
 
-# Evaluate the feature flag for the user
-show_new_homepage = ldclient.get().variation(FEATURE_FLAG_KEY, context, False)
+# Store the authenticator object in the session state
+st.session_state["authenticator"] = authenticator
+# Store the config in the session state so it can be updated later
+st.session_state["config"] = config
 
-st.title("Hello, Streamlit!")
-st.write("**User Key:**", user_key)
-st.write("**User Name:**", user_name)
-st.write("**User Email:**", user_email)
-st.write("**User Geneder:**", user_gender)
+# Authentication logic
+try:
+    authenticator.login(location="main", key="launch-darkly-test-app-home")
+except LoginError as e:
+    st.error(e)
 
-if show_new_homepage:
-    # Render the new homepage
-    st.write("🆕 This is the new homepage 🆕")
+if st.session_state["authentication_status"]:
+    authenticator.logout(location="sidebar", key="launch-darkly-test-app-home")
+
+    # Define a feature flag key
+    FEATURE_FLAG_KEY = "new-homepage"
+
+    # Get the user's information from Streamlit Authenticator
+    app_username = st.session_state.username
+    user_email = st.session_state.email
+    user_name = st.session_state.name
+    user_key = st.session_state["config"]["credentials"]["usernames"][app_username]["id"]
+    user_gender = st.session_state["config"]["credentials"]["usernames"][app_username]["gender"]
+
+    # Build the user context for LaunchDarkly
+    builder = Context.builder(user_key)
+    builder.kind("user")
+    builder.name(user_name)
+    builder.set("email", user_email)
+    builder.set("gender", user_gender)
+    context = builder.build()
+
+    # Evaluate the feature flag for the user
+    show_new_homepage = ldclient.get().variation(FEATURE_FLAG_KEY, context, False)
     
-else:
-    # Render the old homepage
-    st.write("📠 This is the old homepage 📠")
+    st.write("**User Key:**", user_key)
+    st.write("**User Name:**", user_name)
+    st.write("**User Email:**", user_email)
+    st.write("**User Geneder:**", user_gender)
 
-left, right = st.columns(2)
-if left.button("Left button", icon="😁", key="left-button", type="secondary"):
-    left.write("You clicked the left button!")
-if right.button("Right button", icon="😢", key="right-button", type="primary"):
-    right.write("You clicked the right button!")
+    if show_new_homepage:
+        # Render the new homepage
+        st.write("🆕 This is the new homepage 🆕")
+
+    else:
+        # Render the old homepage
+        st.write("📠 This is the old homepage 📠")
+
+    left, right = st.columns(2)
+    if left.button("Left button", icon="😁", key="left-button", type="secondary"):
+        left.write("You clicked the left button!")
+    if right.button("Right button", icon="😢", key="right-button", type="primary"):
+        right.write("You clicked the right button!")
+
+
+elif st.session_state["authentication_status"] is False:
+    st.error("Username/password is incorrect")
+elif st.session_state["authentication_status"] is None:
+    st.warning("Please enter your username and password")
+
+
+
+with st.expander("Session State for Debugging", icon="💾"):
+    st.session_state
+    
+# with open('config.yaml', 'w') as file:
+#     yaml.dump(config, file, default_flow_style=False)
